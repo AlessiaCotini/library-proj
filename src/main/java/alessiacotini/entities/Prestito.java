51bf78ff7611e74ee1bc81a21d973e9efb6de131
AlessiaCotini <alessia.cotini@icloud.com>
2026-06-19 11:58:28 +0200

    update date in prestito

--- src/main/java/alessiacotini/entities/Prestito.java
+++ src/main/java/alessiacotini/entities/Prestito.java
@@ -2,6 +2,7 @@ package alessiacotini.entities;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,6 +16,15 @@ public class Prestito {
     @Column(name= "prestito_id")
     @GeneratedValue
     private UUID prestito_id;
+
+    @Column(name="data_inizio")
+    private LocalDate data_inizio;
+
+    @Column(name="data_restituzione_prevista")
+    private LocalDate data_restituzione_prevista;
+
+    @Column(name="data_restituzione_effettiva")
+    private LocalDate data_restituzione_effettiva;
 
     //MOLTI PRESTITI POSSONO ESSERE ADDOCIATI ALL'ID DI UN UTENTE
 
@@ -33,9 +43,12 @@ public class Prestito {
     //COSTRUTTORI
 
     protected Prestito(){}
-    public Prestito(Utente utente, Elemento elemento){
+    public Prestito(Utente utente, Elemento elemento,LocalDate data_inizio,LocalDate data_restituzione_prevista,LocalDate data_restituzione_effettiva ){
         this.utente= utente;
         this.elemento = elemento;
+        this.data_inizio = LocalDate.now();
+        this.data_restituzione_prevista = LocalDate.now().plusDays(30);
+        this.data_restituzione_effettiva = data_restituzione_effettiva;
     }
 
     //GETTER E SETTER
@@ -55,5 +68,29 @@ public class Prestito {
 
     public void setUtente(Utente utente) {
         this.utente = utente;
+    }
+
+    public LocalDate getData_inizio() {
+        return data_inizio;
+    }
+
+    public void setData_inizio(LocalDate data_inizio) {
+        this.data_inizio = data_inizio;
+    }
+
+    public LocalDate getData_restituzione_prevista() {
+        return data_restituzione_prevista;
+    }
+
+    public void setData_restituzione_prevista(LocalDate data_restituzione_prevista) {
+        this.data_restituzione_prevista = data_restituzione_prevista;
+    }
+
+    public LocalDate getData_restituzione_effettiva() {
+        return data_restituzione_effettiva;
+    }
+
+    public void setData_restituzione_effettiva(LocalDate data_restituzione_effettiva) {
+        this.data_restituzione_effettiva = data_restituzione_effettiva;
     }
 }
